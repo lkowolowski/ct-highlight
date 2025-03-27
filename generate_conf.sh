a43a3f9bf2d06e1eeb94b4846faa44ce1ea0a3e5
lkowolowski <cubit-minters.0y@icloud.com>
2025-03-27 16:55:19 -0700

    build script, color definitions, and some generic patterns to colorize

---
+++ generate_conf.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+files_to_include="
+chromaterm-generic.yml
+"
+
+# Start with the base
+cat chromaterm-colors.yml >chromaterm.yml
+echo "rules:" >>chromaterm.yml
+
+# Add in all the other files we want to include
+for file in ${files_to_include}; do
+  # Check if the file exists, and if so, append it to our config file
+  if [ -e "${file}" ]; then
+    cat "${file}" >>chromaterm.yml
+  fi
+done
